cfbb5de045f985224ad4857f0509ffd63d41ffd9
michael-doll <MWDoll88@gmail.com>
2019-12-02 14:00:51 -0500

    Lost previous project, starting again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.*;
 
 public class Main {
 
@@ -8,9 +11,22 @@ public class Main {
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
         return result;
     }
-
+    //clean up text file to readable lines
+    public static ArrayList<String> cleanText(String junk){
+        ArrayList<String> lines = new ArrayList<String>();
+        Pattern linePattern = Pattern.compile("[^##]*", Pattern.CASE_INSENSITIVE);
+        Matcher lineMatcher = linePattern.matcher(junk);
+        while(lineMatcher.find()){
+            lines.add(lineMatcher.group());
+        }
+        for (int i = 0; i <lines.size() ; i++) {
+            System.out.println(lines.get(i));
+        }
+        return lines;
+    }
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
+        cleanText(output);
         System.out.println(output);
 
     }
